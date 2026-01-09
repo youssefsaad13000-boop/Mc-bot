9bb2c8874e7687de962bb813231cea530d6dda70
youssefsaad13000-boop <youssefsaad13000@gmail.com>
2026-01-10 00:17:31 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,33 @@
+const mineflayer = require('mineflayer');
+
+function startBot() {
+  const bot = mineflayer.createBot({
+    host: 'Teibaceaft123.aternos.me',
+    port: 60036,
+    username: '2bv4t',
+    auth: 'offline'
+  });
+
+  bot.on('spawn', () => {
+    console.log('✅ Bot is online (AFK)');
+
+    // حركة خفيفة عشان ما ينفصل
+    setInterval(() => {
+      bot.setControlState('jump', true);
+      setTimeout(() => {
+        bot.setControlState('jump', false);
+      }, 500);
+    }, 60000);
+  });
+
+  bot.on('end', () => {
+    console.log('❌ Disconnected... reconnecting in 5s');
+    setTimeout(startBot, 5000);
+  });
+
+  bot.on('error', err => {
+    console.log('⚠️ Error:', err);
+  });
+}
+
+startBot();
