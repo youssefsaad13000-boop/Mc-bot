05ae51b0daa10320c52c4c6bec5d9b84e3fcef39
youssefsaad13000-boop <youssefsaad13000@gmail.com>
2026-02-25 23:44:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const mineflayer = require('mineflayer');
 function startBot() {
   const bot = mineflayer.createBot({
     host: 'play.ashpvp.xyz',
-    username: 'hii', // اسم مختلف عن حسابك الأساسي
+    username: 'nothii', // اسم مختلف عن حسابك الأساسي
     auth: 'offline',          // للسيرفرات الـ cracked
     version: '1.20.1'         // أو false لو عايز يتعرف تلقائيًا
   });
@@ -12,7 +12,7 @@ function startBot() {
     console.log('البوت متصل (AFK)');
 
     // تسجيل الحساب أوّل ما يدخل
-    bot.chat('/login 123yyyuuu');
+    bot.chat('/reg 123yyyuuu 123yyyuuu');
 
     // يمشي للأمام باستمرار
     bot.setControlState('forward', true);
