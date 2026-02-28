e782c26d85a99edf5e426c18961514d2bbfa345f
youssefsaad13000-boop <youssefsaad13000@gmail.com>
2026-02-28 21:04:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const mineflayer = require('mineflayer');
 function startBot() {
   const bot = mineflayer.createBot({
     host: 'Drainland.aternos.me',
-    username: 'shay', // اسم مختلف عن حسابك الأساسي
+    username: 'anabot', // اسم مختلف عن حسابك الأساسي
     auth: 'offline',          // للسيرفرات الـ cracked
     version: '1.20.1'         // أو false لو عايز يتعرف تلقائيًا
   });
