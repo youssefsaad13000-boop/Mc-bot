0a4121e2ba936ac7522c9aa1465547ab3a39cbeb
youssefsaad13000-boop <youssefsaad13000@gmail.com>
2026-02-28 21:09:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ function startBot() {
     host: 'Drainland.aternos.me',
     username: 'shay', // اسم مختلف عن حسابك الأساسي
     auth: 'offline',          // للسيرفرات الـ cracked
-    version: '1.21.4'         // أو false لو عايز يتعرف تلقائيًا
+    version: 'false'         // أو false لو عايز يتعرف تلقائيًا
   });
 
   bot.on('spawn', () => {
