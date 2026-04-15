4401c1f433e687e72bc203a8e311e28cf28a03b0
youssefsaad13000-boop <youssefsaad13000@gmail.com>
2026-04-15 14:19:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ function startBot() {
     console.log('البوت متصل (AFK)');
 
     // تسجيل الحساب أوّل ما يدخل
-    bot.chat('/register 123456789yyyuuu 123456789yyyuuu');
+    bot.chat('/login 123456789yyyuuu');
 
     // يمشي للأمام باستمرار
     bot.setControlState('forward', true);
