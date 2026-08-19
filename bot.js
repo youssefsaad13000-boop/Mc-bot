13f3630bc4e9598c8d7813f3e1aa04497d5a2a98
youssefsaad13000-boop <youssefsaad13000@gmail.com>
2026-08-20 02:10:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ const mineflayer = require('mineflayer');
 
 function startBot() {
   const bot = mineflayer.createBot({
-    host: 'enderglider.falixsrv.me',
-    username: 'hoss_2013g',   // اسم جديد للبوت
+    host: 'thsheshcraft.aternos.me',
+    username: 'StormBreaker',   // اسم جديد للبوت
     auth: 'offline',
     version: '1.20.1'           // زي ما كان
   });
